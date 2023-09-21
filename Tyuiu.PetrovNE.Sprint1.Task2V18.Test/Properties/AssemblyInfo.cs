be9f2bb368be791f564840c0031b702a2abba31b
PetrovNikitaEvg <Shug1zawa@yandex.ru>
2023-09-21 23:36:15 +0500

    Task2

---
+++ Tyuiu.PetrovNE.Sprint1.Task2V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PetrovNE.Sprint1.Task2V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PetrovNE.Sprint1.Task2V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dfbf4397-fbdf-4dd7-8f9c-8323c56f8866")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PetrovNE.Sprint1.Task2V18/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PetrovNE.Sprint1.Task2V18.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//выполняет указанные расчёты и печатает результат на экране.
+
+//Расчёты: Известны длины стороны параллелепипеда.
+//Вычислить площадь боковой поверхности параллелепипеда.
+
+//Ввод:
+//Длина параллелепипеда (целое число), Ширина параллелепипеда (целое число),Высота параллелепипеда (целое число)
+
+//Вывод: Площадь боковой поверхности параллелепипеда (целое число)
+
+namespace Tyuiu.PetrovNE.Sprint1.Task2V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                     *");
+            Console.WriteLine("* Тема: Базовые навыки работы в c#                                              *");
+            Console.WriteLine("* Задание #2                                                                    *");
+            Console.WriteLine("* Вариант #18                                                                   *");
+            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                              *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                      *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,       *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает площадь боковой поверхности на экране. *");
+            Console.WriteLine("*                                                                               *");
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                              *");
+            Console.WriteLine("*********************************************************************************");
+
+            int x, y, z;
+            Console.WriteLine("Введите значение X =");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y =");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Z =");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                    *");
+            Console.WriteLine("*********************************************************************************");
+
+            Console.WriteLine("Площадь боковой поверхности параллелепипеда = " + ds.CalculateSideSquareParallelepiped(x, y, z));
+
+            Console.ReadKey();
+
+        }
+    }
+}
